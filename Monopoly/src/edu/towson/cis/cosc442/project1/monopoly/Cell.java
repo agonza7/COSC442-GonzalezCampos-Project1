864b18b9405b6a9528fd11a5f8fd0460ec842602
Andy Gonzalez <agonza7@students.towson.edu>
2018-02-14 03:03:13 -0500

    Task14 Completed

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -37,7 +37,7 @@ public abstract class Cell implements IOwnable {
 		return available;
 	}
 	
-	public abstract void playAction();
+	public abstract boolean playAction(String msg);
 
 	public void setAvailable(boolean available) {
 		this.available = available;
